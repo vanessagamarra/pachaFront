5496e884a2a53b0dc25b77a4a224463daedb7e82
Omar Augusto Collado Vargas <PEX0000433@prosegur.com>
2023-04-30 01:15:01 -0500

    add avance preguntas js

--- Hackaton08/ocollado/retos/reto_17.js
+++ Hackaton08/ocollado/retos/reto_17.js
@@ -9,30 +9,34 @@ function ejecutarReto17() {
     let horas = Utils.ejecutarPromptYParsearAEntero(MENSAJE_PROMPT_RETO_17.HORAS);
     let minutos = Utils.ejecutarPromptYParsearAEntero(MENSAJE_PROMPT_RETO_17.MINUTOS);
     let segundos = Utils.ejecutarPromptYParsearAEntero(MENSAJE_PROMPT_RETO_17.SEGUNDOS);
-    let horaIngresada = `${horas}:${minutos}:${segundos}`
+    let horaIngresada = `${parseZeroToDoubleZero(horas)}:${parseZeroToDoubleZero(minutos)}:${parseZeroToDoubleZero(segundos)}`;
 
-    if (horas > 24 || minutos > 60 || segundos > 60) {
+    if (horas > 23 || minutos > 59 || segundos > 59) {
         alert('Ingrese una hora/minuto/segundo válido');
         return;
     }
 
     segundos = segundos + 1;
  
-    if (segundos > 60) {
+    if (segundos > 59) {
         segundos = 0;
         minutos = minutos + 1;
     }
 
-    if (minutos > 60) {
+    if (minutos > 59) {
         minutos = 0;
         horas = horas + 1;
     }
 
-    if (horas > 24) {
+    if (horas > 23) {
         horas = 0;
     }
 
-    let nuevoHorario = `${horas}:${minutos}:${segundos}`;
+    let nuevoHorario = `${parseZeroToDoubleZero(horas)}:${parseZeroToDoubleZero(minutos)}:${parseZeroToDoubleZero(segundos)}`;
 
     alert(`Usted ingresó: ${horaIngresada}. Y dentro de un segundo la hora será: ${nuevoHorario}`);
+}
+
+function parseZeroToDoubleZero(tiempo) {
+    return tiempo.toString().length === 1 ? `0${tiempo}` : tiempo.toString();
 }
