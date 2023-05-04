8383be2a0bca1aedbb1cf0fd80d8c59a5cdd2e5b
Roberto Pineda <rpineda@x-codec.net>
2023-05-03 20:46:17 -0500

    Ejercicio 2 y 3

--- Semana09Sesion01/rpineda/js/scripts.js
+++ Semana09Sesion01/rpineda/js/scripts.js
@@ -9,10 +9,57 @@ function ej01(){
     else{
         res.innerHTML = "No tiene 3 digitos"
     }
-
-    
-    
 }
 
+function ej02(){
+    //Hacer un algoritmo en Pseint y JavaScript que lea un número entero por el teclado y determinar si es negativo.
+    data = document.getElementById("imp02");
+    res = document.getElementById("res02");
+
+    try {
+        numero = parseInt(data.value);
+        let result = Number.isNaN( numero ); 
+        if(!result){
+            if(numero < 0){
+                res.innerHTML = "es un numero negativo"
+            }
+            else {
+                res.innerHTML = "es un numero positivo"
+            }
+        }
+        else{
+            res.innerHTML = "Los datos ingresados no son numericos"
+        }
+    } catch (error) {
+        throw "No se puede comvertir a numero"
+    }
+    
+}
+function ej03(){
+    //Hacer un algoritmo en Pseint y JavaScript que lea un número y determinar si termina en 4.
+    data = document.getElementById("imp03");
+    res = document.getElementById("res03");
+
+    try {
+        numero = data.value
+        intArr = numero.split('');
+        ultimo = intArr[(intArr.length -1)]
+        if(ultimo == 4){
+            res.innerHTML = "Si ttermina en 4"
+        }
+        else{
+            res.innerHTML = "No termina en 4"
+        }
+
+
+    } catch (error) {
+        throw error
+    }
+    
+}
 button01 = document.getElementById("ej01");
 button01.addEventListener("click", ej01);
+button02 = document.getElementById("ej02");
+button02.addEventListener("click", ej02);
+button03 = document.getElementById("ej03");
+button03.addEventListener("click", ej03);
