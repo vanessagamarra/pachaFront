bd99a9b267bc498ed323e5132825523660cacb5b
Jhosep Velasque <2020101139@unfv.edu.pe>
2023-05-06 15:33:08 -0500

    agregando reto 2

--- Hackaton09/jvelasque/app.js
+++ Hackaton09/jvelasque/app.js
@@ -29,38 +29,180 @@ var miServidor = http.createServer((peticion, respuesta) => {
                 .replace("%respuesta%", answ);
             respuesta.end(html)
             break;
-        case "/ejercicio/2":
-            titulo = "Ejercicio 01"
-            parrafo = "Un estudiante realiza 4 exámenes, calcular el promedio de estos"
-            answ = ejercicio02(20,14,12,15)
-            var html = fs.readFileSync(__dirname + "/templates/ejercicio.html")
-                .toString()
-                .replace("%titulo%", titulo)
-                .replace("%parrafo%", parrafo)
-                .replace("%respuesta%", answ);
-            respuesta.end(html)
-            break;
-        case "/ejercicio/3":
-            respuesta.end("Inicio")
-            break;
-        case "/ejercicio/4":
-            respuesta.end("Inicio")
-            break;
-        case "/ejercicio/5":
-            respuesta.end("Inicio")
-            break;
-        case "/ejercicio/6":
-            respuesta.end("Inicio")
-            break;
-        case "/ejercicio/7":
-            respuesta.end("Inicio")
-            break;
-        case "/ejercicio/8":
-            respuesta.end("Inicio")
-            break;
-        case "/ejercicio/9":
-            respuesta.end("Inicio")
-            break;
+            case '/forms/form-ejercicio-3':
+                var html = fs.readFileSync(__dirname + '/forms/form3.html').toString();
+                respuesta.end(html);
+                break;
+            case '/forms/form-ejercicio-4':
+                var html = fs.readFileSync(__dirname + '/forms/form4.html').toString();
+                respuesta.end(html);
+                break;
+            case '/forms/form-ejercicio-5':
+                var html = fs.readFileSync(__dirname + '/forms/form5.html').toString();
+                respuesta.end(html);
+                break;
+            case '/forms/form-ejercicio-6':
+                var html = fs.readFileSync(__dirname + '/forms/form6.html').toString();
+                respuesta.end(html);
+                break;
+            case '/forms/form-ejercicio-7':
+                var html = fs.readFileSync(__dirname + '/forms/form7.html').toString();
+                respuesta.end(html);
+                break;
+            case '/forms/form-ejercicio-8':
+                var html = fs.readFileSync(__dirname + '/forms/form8.html').toString();
+                respuesta.end(html);
+                break;
+            case '/forms/form-ejercicio-9':
+                var html = fs.readFileSync(__dirname + '/forms/form9.html').toString();
+                respuesta.end(html);
+                break;
+            case '/forms/form-ejercicio-10':
+                var html = fs.readFileSync(__dirname + '/forms/form10.html').toString();
+                respuesta.end(html);
+                break;
+            case '/forms/form-ejercicio-11':
+                var html = fs.readFileSync(__dirname + '/forms/form11.html').toString();
+                respuesta.end(html);
+                break;
+            // case '/forms/form-ejercicio-12':
+            // 	var html = fs.readFileSync(__dirname + '/forms/form12.html').toString();
+            // 	respuesta.end(html);
+            // 	break;
+            case '/forms/form-ejercicio-13':
+                var html = fs.readFileSync(__dirname + '/forms/form13.html').toString();
+                respuesta.end(html);
+                break;
+            case '/forms/form-ejercicio-14':
+                var html = fs.readFileSync(__dirname + '/forms/form14.html').toString();
+                respuesta.end(html);
+                break;
+            case '/forms/form-ejercicio-15':
+                var html = fs.readFileSync(__dirname + '/forms/form15.html').toString();
+                respuesta.end(html);
+                break;
+            case '/ejercicio/1':
+                titulo = 'Ejercicio 01';
+                parrafo = '1.	Implementar un algoritmo que reciba 2 argumentos y los sume, el resultado se deberá imprimir en pantalla';
+                answ = ejercicio01(parseInt(urlObject.query.num1), parseInt(urlObject.query.num2));
+                var html = fs
+                    .readFileSync(__dirname + '/templates/ejercicio.html')
+                    .toString()
+                    .replace('%titulo%', titulo)
+                    .replace('%parrafo%', parrafo)
+                    .replace('%respuesta%', answ);
+                respuesta.end(html);
+                break;
+            case '/ejercicio/2':
+                titulo = 'Ejercicio 02';
+                parrafo = 'Un estudiante realiza 4 exámenes, calcular el promedio de estos';
+                answ = ejercicio02(
+                    parseInt(urlObject.query.nota1),
+                    parseInt(urlObject.query.nota2),
+                    parseInt(urlObject.query.nota3),
+                    parseInt(urlObject.query.nota4)
+                );
+                var html = fs
+                    .readFileSync(__dirname + '/templates/ejercicio.html')
+                    .toString()
+                    .replace('%titulo%', titulo)
+                    .replace('%parrafo%', parrafo)
+                    .replace('%respuesta%', answ);
+                respuesta.end(html);
+                break;
+            case '/ejercicio/3':
+                titulo = 'Ejercicio 03';
+                parrafo = 'Calcular el área de un rectángulo';
+                // ejercicio03(base, altura);
+                answ = `${ejercicio03(parseInt(urlObject.query.base), parseInt(urlObject.query.altura))}cm`;
+                var html = fs
+                    .readFileSync(__dirname + '/templates/ejercicio.html')
+                    .toString()
+                    .replace('%titulo%', titulo)
+                    .replace('%parrafo%', parrafo)
+                    .replace('%respuesta%', answ);
+                respuesta.end(html);
+                break;
+            case '/ejercicio/4':
+                titulo = 'Ejercicio 04';
+                parrafo = 'Calcular el área de un triángulo';
+                // ejercicio04(base, altura);
+                answ = `${ejercicio04(parseInt(urlObject.query.base), parseInt(urlObject.query.altura))}cm`;
+                var html = fs
+                    .readFileSync(__dirname + '/templates/ejercicio.html')
+                    .toString()
+                    .replace('%titulo%', titulo)
+                    .replace('%parrafo%', parrafo)
+                    .replace('%respuesta%', answ);
+                respuesta.end(html);
+                break;
+            case '/ejercicio/5':
+                titulo = 'Ejercicio 05';
+                parrafo = 'Calcular el área de un circunferencia';
+                // ejercicio05(radio);
+                answ = `${ejercicio05(parseInt(urlObject.query.radio))}cm²`;
+                var html = fs
+                    .readFileSync(__dirname + '/templates/ejercicio.html')
+                    .toString()
+                    .replace('%titulo%', titulo)
+                    .replace('%parrafo%', parrafo)
+                    .replace('%respuesta%', answ);
+                respuesta.end(html);
+                break;
+            case '/ejercicio/6':
+                titulo = 'Ejercicio 06';
+                parrafo = 'Determinar el sueldo semanal de un trabajador basándose en sus horas trabajadas y su salario de hora';
+                // ejercicio06(horas, sueldoxhora);
+                answ = `S/${ejercicio06(parseInt(urlObject.query.horas), parseInt(urlObject.query.sueldo))}`;
+                var html = fs
+                    .readFileSync(__dirname + '/templates/ejercicio.html')
+                    .toString()
+                    .replace('%titulo%', titulo)
+                    .replace('%parrafo%', parrafo)
+                    .replace('%respuesta%', answ);
+                respuesta.end(html);
+                break;
+            case '/ejercicio/7':
+                titulo = 'Ejercicio 07';
+                parrafo =
+                    'Una modista, para realizar sus prendas de vestir, encarga las telas al extranjero. Para cada pedido, tiene que proporcionar las medidas de la tela en pulgadas, pero ella generalmente las tiene en metros. Realice un algoritmo para ayudar a resolver el problema, determinando cuantas pulgadas debe pedir con base en los metros que requiere. (1 pulgada = 0.0254 m)';
+                // ejercicio07(metros);
+                answ = `${ejercicio07(parseInt(urlObject.query.metros))} pulgadas`;
+                var html = fs
+                    .readFileSync(__dirname + '/templates/ejercicio.html')
+                    .toString()
+                    .replace('%titulo%', titulo)
+                    .replace('%parrafo%', parrafo)
+                    .replace('%respuesta%', answ);
+                respuesta.end(html);
+                break;
+            case '/ejercicio/8':
+                titulo = 'Ejercicio 08';
+                parrafo = 'Una empresa importadora desea determinar cuántos dólares puede adquirir con equis cantidad de dinero peruano';
+                // ejercicio08(precio dolar /soles a convertir);
+                answ = `$${ejercicio08(parseInt(urlObject.query.dolar), parseInt(urlObject.query.soles))}`;
+                var html = fs
+                    .readFileSync(__dirname + '/templates/ejercicio.html')
+                    .toString()
+                    .replace('%titulo%', titulo)
+                    .replace('%parrafo%', parrafo)
+                    .replace('%respuesta%', answ);
+                respuesta.end(html);
+                break;
+            case '/ejercicio/9':
+                titulo = 'Ejercicio 09';
+                parrafo =
+                    'Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo, pero cuando se les realiza la entrevista sólo se les pregunta el año en que nacieron';
+                // ejercicio09(Año de nacimiento);
+                answ = `Tiene ${ejercicio09(parseInt(urlObject.query.year))} años.`;
+                var html = fs
+                    .readFileSync(__dirname + '/templates/ejercicio.html')
+                    .toString()
+                    .replace('%titulo%', titulo)
+                    .replace('%parrafo%', parrafo)
+                    .replace('%respuesta%', answ);
+                respuesta.end(html);
+                break;
         case "/ejercicio/10":
             respuesta.end("Inicio")
             break;
