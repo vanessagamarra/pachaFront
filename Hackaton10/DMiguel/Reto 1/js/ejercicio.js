20b55bbaef381bafe54a9e67b54f1788f21149af
pana-miguel <miguelangel.dominguez@unheval.pe>
2023-05-13 15:05:33 -0500

    Se agregaron los ejercicios 3 - 5, ademas se creo la funcion createInputN que se encarga de crear input and label segun se el ejercicio

--- Hackaton10/DMiguel/Reto 1/js/ejercicio.js
+++ Hackaton10/DMiguel/Reto 1/js/ejercicio.js
@@ -21,6 +21,18 @@ function createContentEjercicio(numberEjer = "",problem = "",etiquetas = "", can
     contentEjercicio.innerHTML = etiquetas;
 }
 
+function createInputsN(){
+    const text = '<label for="dato_i">Numero _i</label><input id="dato_i" type="text" class="form-control">'
+    const createInputN = document.getElementById("form-ejercicio");
+    const countInput = parseInt(document.getElementById("createInput").value);
+    let resultado = "";
+    for (let i = 1; i <= countInput; i++) {
+        resultado += text.replace(/_i/g, `${i}`);
+    }
+    createInputN.innerHTML = resultado;
+    return countInput;
+}
+
 function ejercicio_1() {
     let dato1 = parseInt(document.getElementById('dato1').value);
     let dato2 = parseInt(document.getElementById('dato2').value);
@@ -36,10 +48,22 @@ function ejercicio_2() {
 }
 
 function ejercicio_3() {
+    i=1;
+    let rpta = 0
+    while (document.getElementById("dato"+i)) {
+        numero = parseInt(document.getElementById("dato"+i).value);
+        console.log(numero);
+        rpta += numero**3;
+        i++;
+    }
+    rptaEjercicio.innerText = `El resultado de la suma de los numeros al cubo es ${rpta}.`;
+}
+
+function ejercicio_4    () {
     let dato1 = parseInt(document.getElementById('dato1').value);
     let dato2 = parseInt(document.getElementById('dato2').value);
-    let rpta = dato1**dato2;
-    rptaEjercicio.innerText = `La potencia al ${dato2} de ${dato1} es ${rpta}.`;
+    let rpta = dato1*dato2/2;
+    rptaEjercicio.innerText = `El area del triangulo es ${rpta}.`;
 }
 
 const etiquetas = '<label for="dato_i">Datos _i</label><input id="dato_i" type="text" class="form-control">'
@@ -63,14 +87,24 @@ switch (nombreEjercicio) {
     case 'ejercicio3':
         console.log("Estamos en el ejercicio 3");
         problem = "Crea una función que tome números y devuelva la suma de sus cubos. sumOfCubes(1, 5, 9) ➞ 855 // Since 1^3 + 5^3 + 9^3 = 1 + 125 + 729 = 855";
-        createContentEjercicio(nombreEjercicio.toUpperCase(), problem,etiquetas,2);
+        espEtiqueta = '<label for="createInput">Cantidad de Numeros</label><input id="createInput" type="text" class="form-control"><a id="newInputs" class="btn btn-primary">Crear</a>'
+        createContentEjercicio(nombreEjercicio.toUpperCase(), problem,espEtiqueta,1);
+        const newInput = document.getElementById("newInputs");
+
+        newInput.addEventListener('click',createInputsN);
         ejecutar.addEventListener('click', ejercicio_3);
         break;
     case 'ejercicio4':
+        problem = "Escribe una función que tome la base y la altura de un triángulo y devuelva su área. triArea(3, 2) ➞ 3";
+        createContentEjercicio(nombreEjercicio.toUpperCase(), problem,etiquetas,2);
+        ejecutar.addEventListener('click', ejercicio_4);
         console.log("Estamos en el ejercicio 4");
         break;
     case 'ejercicio5':
         console.log("Estamos en el ejercicio 5");
+        problem = "Crea una función llamada calculator que recibe 3 parámetros, dos números y una operación matemática (+,-,/,x,%), y si la operación no es correcta que envié un mensaje “El parámetro no es reconocido” calculator(2,"+", 2) ➞ 4";
+        createContentEjercicio(nombreEjercicio.toUpperCase(), problem,etiquetas,3);
+        // ejecutar.addEventListener('click', ejercicio_5);
         break;
     default:
         console.log('Ejercicio no reconocido');
