5b18e915842dd388ab9cfea6089634a0dfcf10ea
MiguelDominguez <miguelangel.dominguez@unheval.pe>
2023-05-14 19:12:24 -0500

    Se agrego la funcion getPlaceholderArray para agregar un placeholder en varios input por un for, ademas se termino la funcion ejercicio 5

--- Hackaton10/DMiguel/Reto 1/js/ejercicio.js
+++ Hackaton10/DMiguel/Reto 1/js/ejercicio.js
@@ -14,6 +14,12 @@ function repeatText(text, count) {
     return resultado;
 }
 
+function getPlaceholderArray(texto = [], id ="id"){
+    for (let i = 1; i < texto.length+1; i++) {
+        document.getElementById(id + i.toString()).placeholder = texto[i-1];
+    }
+}
+
 function createContentEjercicio(numberEjer = "",problem = "",etiquetas = "", cantidad = 2) {
     titleEjercicio.textContent = numberEjer;
     problemEjercicio.textContent = problem;
@@ -59,11 +65,39 @@ function ejercicio_3() {
     rptaEjercicio.innerText = `El resultado de la suma de los numeros al cubo es ${rpta}.`;
 }
 
-function ejercicio_4    () {
+function ejercicio_4() {
     let dato1 = parseInt(document.getElementById('dato1').value);
     let dato2 = parseInt(document.getElementById('dato2').value);
     let rpta = dato1*dato2/2;
     rptaEjercicio.innerText = `El area del triangulo es ${rpta}.`;
+}
+
+function ejercicio_5() {
+    let dato1 = parseInt(document.getElementById('dato1').value);
+    let operacion= document.getElementById('dato2').value;
+    let dato2 = parseInt(document.getElementById('dato3').value);
+    let resultado = 0;
+    switch (operacion) {
+        case "+":
+            resultado = dato1 + dato2;
+            break;
+        case "-":
+            resultado = dato1 - dato2;
+            break;
+        case "/":
+            resultado = dato1 / dato2;
+            break;
+        case "*":
+            resultado = dato1 * dato2;
+            break;
+        case "%":
+            resultado = dato1 % dato2
+            break;
+        default:
+            resultado = "La operacion ingresada es desconocida";
+            break;
+    }
+    rptaEjercicio.innerText = `El resultado de la operacion es  ${resultado}.`;
 }
 
 const etiquetas = '<label for="dato_i">Datos _i</label><input id="dato_i" type="text" class="form-control">'
@@ -90,7 +124,6 @@ switch (nombreEjercicio) {
         espEtiqueta = '<label for="createInput">Cantidad de Numeros</label><input id="createInput" type="text" class="form-control"><a id="newInputs" class="btn btn-primary">Crear</a>'
         createContentEjercicio(nombreEjercicio.toUpperCase(), problem,espEtiqueta,1);
         const newInput = document.getElementById("newInputs");
-
         newInput.addEventListener('click',createInputsN);
         ejecutar.addEventListener('click', ejercicio_3);
         break;
@@ -104,7 +137,8 @@ switch (nombreEjercicio) {
         console.log("Estamos en el ejercicio 5");
         problem = "Crea una función llamada calculator que recibe 3 parámetros, dos números y una operación matemática (+,-,/,x,%), y si la operación no es correcta que envié un mensaje “El parámetro no es reconocido” calculator(2,"+", 2) ➞ 4";
         createContentEjercicio(nombreEjercicio.toUpperCase(), problem,etiquetas,3);
-        // ejecutar.addEventListener('click', ejercicio_5);
+        getPlaceholderArray(["Numero 1","Signo de Operacion","Numero 2"],"dato");
+        ejecutar.addEventListener('click', ejercicio_5);
         break;
     default:
         console.log('Ejercicio no reconocido');
