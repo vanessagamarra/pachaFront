743f7b8be30e35b1b8f5cc25d921e1ad52a0545d
MiguelDominguez <miguelangel.dominguez@unheval.pe>
2023-05-15 14:50:26 -0500

    Se agrego funciones para pocesar peticiones cliente -> servidor

--- Hackaton10/DMiguel/Reto 2/app.js
+++ Hackaton10/DMiguel/Reto 2/app.js
@@ -1,100 +1,100 @@
 var fs = require("fs");
 // llamamos a la librería http
 var http = require("http");
-var url = require('url'); 
-const querystring = require('querystring');
-
+var url = require("url");
+const querystring = require("querystring");
 
 var titulo = "Hackaton 10";
 var parrafo = "Respuestas de la hackaton 09";
-var new_inputs = '<label for="dato_i">Datos _i</label><input id="dato_i" type="text" class="form-control">'
+var new_inputs ='<label for="dato_i">Datos _i</label><input id="dato_i" type="text" class="form-control">';
+
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var miServidor = http.createServer((peticion, respuesta) => {
-    respuesta.writeHead(200, { "Content-Type": "text/html" });
-    switch (peticion.url) {
-        case "/":
-            var html = fs
-                .readFileSync(__dirname + "/templates/inicio.html")
-            respuesta.end(html);
-            break;
-        case "/ejercicio/1":
-            titulo = "Ejercicio 01";
-            parrafo = "1. Implementar un algoritmo que reciba 2 argumentos y los sume, el resultado se deberá imprimir en pantalla";
-            generarEjercicio(respuesta, titulo, parrafo, new_inputs, 2);
-            resultado = leerFormulario(peticion, (formulario) => {
-                // Aquí ya tienes los datos del formulario en un objeto
-                const num1 = parseInt(formulario.num1);
-                const num2 = parseInt(formulario.num2);
-                
-                // Suma los números
-                const resultado = num1 + num2;
-                return resultado
-                
-            });
-            // respuesta.write(`<p>El resultado de la suma es: ${resultado}</p>`);
-            respuesta.end();
-            break;
-        case "/ejercicio/2":
-            titulo = "Ejercicio 02"
-            parrafo = "2. Un estudiante realiza 4 exámenes, calcular el promedio de estos"
-            generarEjercicio(respuesta, titulo, parrafo, new_inputs, 4);
-            break;
-        case "/ejercicio/3":
-            titulo = "Ejercicio 03"
-            parrafo = "3. Calcular el área de un rectángulo"
-            generarEjercicio(respuesta, titulo, parrafo, new_inputs, 2);
-            break;
-        case "/ejercicio/4":
-            titulo = "Ejercicio 04"
-            parrafo = "4. Calcular el área de un triángulo"
-            generarEjercicio(respuesta, titulo, parrafo, new_inputs, 2);
-            break;
-        case "/ejercicio/5":
-            titulo = "Ejercicio 05"
-            parrafo = "5. Calcular el área de una circunferencia"
-            generarEjercicio(respuesta, titulo, parrafo, new_inputs, 1);
-            break;
-        default:
-            respuesta.writeHead(404, { "Content-Type": "text/html" });
-            respuesta.end("404 not found")
-            break;
-    }
+  respuesta.writeHead(200, { "Content-Type": "text/html" });
+  switch (peticion.url) {
+    case "/":
+      var html = fs.readFileSync(__dirname + "/templates/inicio.html");
+      respuesta.end(html);
+      break;
+    case "/ejercicio/1":
+      titulo = "Ejercicio 01";
+      parrafo = "1. Implementar un algoritmo que reciba 2 argumentos y los sume, el resultado se deberá imprimir en pantalla";
+      generarEjercicio(respuesta, titulo, parrafo, new_inputs, 2, peticion.url);
+      
+      break;
+    case "/ejercicio/2":
+      titulo = "Ejercicio 02";
+      parrafo =
+        "2. Un estudiante realiza 4 exámenes, calcular el promedio de estos";
+      generarEjercicio(respuesta, titulo, parrafo, new_inputs, 4);
+      break;
+    case "/ejercicio/3":
+      titulo = "Ejercicio 03";
+      parrafo = "3. Calcular el área de un rectángulo";
+      generarEjercicio(respuesta, titulo, parrafo, new_inputs, 2);
+      break;
+    case "/ejercicio/4":
+      titulo = "Ejercicio 04";
+      parrafo = "4. Calcular el área de un triángulo";
+      generarEjercicio(respuesta, titulo, parrafo, new_inputs, 2);
+      break;
+    case "/ejercicio/5":
+      titulo = "Ejercicio 05";
+      parrafo = "5. Calcular el área de una circunferencia";
+      generarEjercicio(respuesta, titulo, parrafo, new_inputs, 1);
+      break;
+    default:
+      respuesta.writeHead(404, { "Content-Type": "text/html" });
+      respuesta.end("404 not found");
+      break;
+  }
 });
 
 // inicializar el servidor
 miServidor.listen(1111, "127.0.0.1");
 
 function repeatText(text, count) {
-    let resultado = "";
-    for (let i = 1; i <= count; i++) {
-        resultado += text.replace(/_i/g, `${i}`);
-    }
-    return resultado;
-}
-
-function generarEjercicio(respuesta, titulo, parrafo, new_inputs, cant_inp) {
-    new_inputs = repeatText(new_inputs, cant_inp)
-    let html = fs.readFileSync(__dirname + "/templates/ejercicio.html")
-        .toString()
-        .replace("%titulo%", titulo)
-        .replace("%parrafo%", parrafo)
-        .replace("%inputs%", new_inputs)
-    respuesta.end(html)
-}
-
-function leerFormulario(peticion, callback) {
-    if (peticion.method === 'POST') {
-      let datosFormulario = '';
-      peticion.on('data', (chunk) => {
-        datosFormulario += chunk;
-      });
-      peticion.on('end', () => {
-        const formulario = querystring.parse(datosFormulario);
-        callback(formulario);
-      });
-    } else {
-      // Si la solicitud no es POST, devuelves un error 405 (Método no permitido)
-      console.log("Error 405");
-      console.log('Método no permitido');
-    }
+  let resultado = "";
+  for (let i = 1; i <= count; i++) {
+    resultado += text.replace(/_i/g, `${i}`);
   }
+  return resultado;
+}
+
+function generarEjercicio(respuesta,titulo,parrafo,new_inputs,cant_inp,domine) {
+  new_inputs = repeatText(new_inputs, cant_inp);
+  new_inputs = `<form id="form-ejercicio" action="${domine}" method="POST">${new_inputs}<button type="submit" id="ejecutar" class="btn btn-primary">Ejecutar</button></form>`;
+  let html = fs
+    .readFileSync(__dirname + "/templates/ejercicio.html")
+    .toString()
+    .replace("%titulo%", titulo)
+    .replace("%parrafo%", parrafo)
+    .replace("%inputs%", new_inputs);
+  respuesta.end(html);
+}
+
+// Define la ruta para mostrar el formulario
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + `/templates/ejercicio/1`);
+    console.log("en ek ")
+});
+
+app.post("/ejercicio/1", (req, res) => {
+    const num1 = parseInt(req.body.dato1);
+    const num2 = parseInt(req.body.dato2);
+    const resultado = num1 + num2;
+    let respuesta = `El resultado es ${resultado}`;
+    console.log(respuesta);
+    res.write(`<div id="respuesta">El resultado es ${respuesta}</div>`);
+    var html = fs
+      .readFileSync(__dirname + `/templates/ejercicio/1`)
+      .toString()
+      .replace("%rpta%", respuesta);
+    res.end(html);
+});
