14983a08420fdf53f6b02f7330ce26d797662b2e
rpineda <rpineda@x-codec.net>
2023-05-15 22:06:54 -0500

    Clase 15052023 parte 2

--- Semana11Sesion01/rpineda/script.js
+++ Semana11Sesion01/rpineda/script.js
@@ -56,4 +56,121 @@ function ejecutar(){
 
     const collection = document.getElementsByTagName("*");
     console.log(collection)
+    let select1  = document.getElementById("select1")
+    select1.addEventListener("change", function(){
+        console.log("cambio de auto")
+    });
+    
 }
+
+let boton = document.getElementById('ejecutar')
+boton.addEventListener("click", ejecutar);
+boton.addEventListener("blur", onblurRP);
+
+
+function onblurRP(){
+    console.log("paso por onblur")
+}
+let email = document.getElementById('email')
+email.addEventListener("copy", function(){
+    console.log("Hizo copy ctrl + c")
+});
+
+let imagen = document.getElementById('zaku3')
+imagen.addEventListener("copy", function(){
+    console.log("Hizo copy ctrl + c")
+});
+
+//pass1
+
+let pass1 = document.getElementById('pass1')
+pass1.addEventListener("paste", function(){
+    console.log("Hizo pegar ctrl + v")
+    alert("Debes digitar la contrasena")
+    document.getElementById('pass1').setAttribute('value','')
+});
+
+//object.onkeydown = function(){myScript};
+let pass2 = document.getElementById('pass2')
+pass2.onkeydown = function(event){
+    console.log(event.keyCode)
+    if (event.keyCode == '16' || event.keyCode == '20'){
+       alert("No puedes poner mayusculas")
+    }
+};
+
+window.onload = function(){ 
+    console.log("Termino de cargar la pagina")
+    img = document.getElementById("zaku3")
+    img.addEventListener("mouseover", mouse);
+};
+
+function mouse(){
+    console.log("Paso el mouse por aqui")
+}
+let formulario = document.getElementById("myform")
+formulario.addEventListener("reset", resetForm);
+function resetForm(){
+    console.log("Reseteo el formulario")
+}
+
+// document.addEventListener("dragstart", function(event) {
+//     // The dataTransfer.setData() method sets the data type and the value of the dragged data
+//     event.dataTransfer.setData("Text", event.target.id);
+  
+//     // Output some text when starting to drag the p element
+//     document.getElementById("demo").innerHTML = "Started to drag the p element.";
+  
+//     // Change the opacity of the draggable element
+//     event.target.style.opacity = "0.4";
+//   });
+  
+//   // While dragging the p element, change the color of the output text
+//   document.addEventListener("drag", function(event) {
+//     document.getElementById("demo").style.color = "red";
+//   });
+  
+//   // Output some text when finished dragging the p element and reset the opacity
+//   document.addEventListener("dragend", function(event) {
+//     document.getElementById("demo").innerHTML = "Finished dragging the p element.";
+//     event.target.style.opacity = "1";
+//   });
+  
+  
+//   // Events fired on the drop target
+  
+//   // When the draggable p element enters the droptarget, change the DIVS's border style
+//   document.addEventListener("dragenter", function(event) {
+//     if ( event.target.className == "droptarget" ) {
+//       event.target.style.border = "3px dotted red";
+//     }
+//   });
+  
+//   // By default, data/elements cannot be dropped in other elements. To allow a drop, we must prevent the default handling of the element
+//   document.addEventListener("dragover", function(event) {
+//     event.preventDefault();
+//   });
+  
+//   // When the draggable p element leaves the droptarget, reset the DIVS's border style
+//   document.addEventListener("dragleave", function(event) {
+//     if ( event.target.className == "droptarget" ) {
+//       event.target.style.border = "";
+//     }
+//   });
+  
+//   /* On drop - Prevent the browser default handling of the data (default is open as link on drop)
+//   Reset the color of the output text and DIV's border color
+//   Get the dragged data with the dataTransfer.getData() method
+//   The dragged data is the id of the dragged element ("drag1")
+//   Append the dragged element into the drop element
+//   */
+//   document.addEventListener("drop", function(event) {
+//     event.preventDefault();
+//     if ( event.target.className == "droptarget" ) {
+//       document.getElementById("demo").style.color = "";
+//       event.target.style.border = "";
+//       var data = event.dataTransfer.getData("Text");
+//       event.target.appendChild(document.getElementById(data));
+//     }
+//   });
+  
