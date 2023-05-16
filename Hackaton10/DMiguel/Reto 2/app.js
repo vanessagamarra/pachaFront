9e82a33471299cf266119faf1c8cb719b23903ee
MiguelDominguez <miguelangel.dominguez@unheval.pe>
2023-05-15 22:08:19 -0500

    Se modifico app.js, de pasar de peticiones por switch a petiones app.get

--- Hackaton10/DMiguel/Reto 2/app.js
+++ Hackaton10/DMiguel/Reto 2/app.js
@@ -1,63 +1,51 @@
 var fs = require("fs");
-// llamamos a la librería http
-var http = require("http");
-var url = require("url");
-const querystring = require("querystring");
-
-var titulo = "Hackaton 10";
-var parrafo = "Respuestas de la hackaton 09";
-var new_inputs = '<label for="dato_i">Datos _i</label><input id="dato_i" name="dato_1" type="text" class="form-control">';
-
 const express = require("express");
-const app = express();
-
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+var new_inputs = '<label for="dato_i">Datos _i</label><input id="dato_i" name="dato_i" type="text" class="form-control">';
 
-var miServidor = http.createServer((peticion, respuesta) => {
-    respuesta.writeHead(200, { "Content-Type": "text/html" });
-    switch (peticion.url) {
-        case "/":
-            var html = fs.readFileSync(__dirname + "/templates/inicio.html");
-            respuesta.end(html);
-            break;
-        case "/ejercicio/1":
-            titulo = "Ejercicio 01";
-            parrafo = "1. Implementar un algoritmo que reciba 2 argumentos y los sume, el resultado se deberá imprimir en pantalla";
-            generarEjercicio(respuesta, titulo, parrafo, new_inputs, 2);
-
-            break;
-        case "/ejercicio/2":
-            titulo = "Ejercicio 02";
-            parrafo =
-                "2. Un estudiante realiza 4 exámenes, calcular el promedio de estos";
-            generarEjercicio(respuesta, titulo, parrafo, new_inputs, 4);
-            break;
-        case "/ejercicio/3":
-            titulo = "Ejercicio 03";
-            parrafo = "3. Calcular el área de un rectángulo";
-            generarEjercicio(respuesta, titulo, parrafo, new_inputs, 2);
-            break;
-        case "/ejercicio/4":
-            titulo = "Ejercicio 04";
-            parrafo = "4. Calcular el área de un triángulo";
-            generarEjercicio(respuesta, titulo, parrafo, new_inputs, 2);
-            break;
-        case "/ejercicio/5":
-            titulo = "Ejercicio 05";
-            parrafo = "5. Calcular el área de una circunferencia";
-            generarEjercicio(respuesta, titulo, parrafo, new_inputs, 1);
-            break;
-        default:
-            respuesta.writeHead(404, { "Content-Type": "text/html" });
-            respuesta.end("404 not found");
-            break;
-    }
+const app = express();
+var puerto = process.env.PUERTO || 3000;
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false,
 });
 
-// inicializar el servidor
-miServidor.listen(1111, "127.0.0.1");
+app.get("/", function (peticion, respuesta) {
+    let titulo = "Hackaton 10";
+    let parrafo = "Alumno: Dominguez Valentin Miguel";
+    var html = fs
+        .readFileSync(__dirname + '/templates/inicio.html')
+        .toString()
+        .replace('%titulo%', titulo)
+        .replace('%parrafo%', parrafo);
+    respuesta.end(html);
+});
+
+app.get("/ejercicio/1/", urlencodedParser, function (peticion, respuesta) {
+    titulo = 'Ejercicio 01';
+    parrafo = '1.	Implementar un algoritmo que reciba 2 argumentos y los sume, el resultado se deberá imprimir en pantalla';
+    console.log(peticion.query.dato1);
+    console.log(peticion.query.dato2);
+    let param1 = parseInt(peticion.query.dato1);
+    let param2 = parseInt(peticion.query.dato2);
+
+    let answ = ejercicio01(param1, param2);
+    generarEjercicio(respuesta, titulo, parrafo, new_inputs, answ, 2)
+});
+
+app.get("/ejercicio/2/", urlencodedParser, function (peticion, respuesta) {
+    titulo = 'Ejercicio 02';
+    parrafo = 'Cree una función que tome números y devuelva la suma de sus cubos.sumOfCubes(1, 5, 9) ➞ 855// Since 1^3 + 5^3 + 9^3 = 1 + 125 + 729 = 855';
+    console.log(peticion.query.dato1);
+    console.log(peticion.query.dato2);
+    let param1 = parseInt(peticion.query.dato1);
+    let param2 = parseInt(peticion.query.dato2);
+
+    answ = ejercicio01(param1, param2);
+    generarEjercicio(respuesta, titulo, parrafo, new_inputs, answ, 5)
+});
+
+app.listen(puerto);
 
 function repeatText(text, count) {
     let resultado = "";
@@ -67,33 +55,24 @@ function repeatText(text, count) {
     return resultado;
 }
 
-function generarEjercicio(respuesta, titulo, parrafo, new_inputs, cant_inp) {
+function generarEjercicio(respuesta, titulo, parrafo, new_inputs,answ, cant_inp) {
     new_inputs = repeatText(new_inputs, cant_inp);
     let html = fs
         .readFileSync(__dirname + "/templates/ejercicio.html")
         .toString()
         .replace("%titulo%", titulo)
         .replace("%parrafo%", parrafo)
-        .replace("%inputs%", new_inputs);
+        .replace("%inputs%", new_inputs)
+        .replace('%respuesta%',answ);
     respuesta.end(html);
 }
 
-// Define la ruta para mostrar el formulario
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + `/templates/ejercicio/1`);
-    console.log("en ek ")
-});
+function ejercicio01(numero1, numero2) {
+    let respuesta = numero1 + numero2;
+    return respuesta;
+}
 
-app.post("/ejercicio/1", (req, res) => {
-    const num1 = parseInt(req.body.dato1);
-    const num2 = parseInt(req.body.dato2);
-    const resultado = num1 + num2;
-    let respuesta = `El resultado es ${resultado}`;
-    console.log(respuesta);
-    res.write(`<div id="respuesta">El resultado es ${respuesta}</div>`);
-    var html = fs
-        .readFileSync(__dirname + `/templates/ejercicio/1`)
-        .toString()
-        .replace("%rpta%", respuesta);
-    res.end(html);
-});
+function ejercicio02(examen1, examen2, examen3, examen4) {
+    let respuesta = (examen1 + examen2 + examen3 + examen4) / 4;
+    return respuesta;
+}
