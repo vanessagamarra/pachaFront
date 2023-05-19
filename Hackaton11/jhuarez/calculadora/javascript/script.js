4b56c2d09501262e347fc20ba6242521d205e140
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2023-05-19 16:53:43 -0500

    hackaton 11, html y css calculadora

---
+++ Hackaton11/jhuarez/calculadora/javascript/script.js
@@ -0,0 +1,5 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const clearEntryButton = document.querySelector('[data-clear-entry]')
+const allClearButton = document.querySelector('[data-all-clear]')
