6f13ee8665bf9137d655ddb2197bba3d8ee8e245
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2023-05-20 13:27:31 -0500

    lista de tareas semi completa

--- Hackaton11/jhuarez/listaTareas/javascript/script.js
+++ Hackaton11/jhuarez/listaTareas/javascript/script.js
@@ -2,18 +2,70 @@ document.addEventListener("DOMContentLoaded", function() {
     var taskForm = document.getElementById('taskForm');
 
     taskForm.addEventListener('submit', function(event) {
+
       event.preventDefault(); // Evitar que el formulario se envíe
 
       // Obtener los valores de los campos de entrada
-      var datepickerValue = document.getElementById('datepicker').value;
-      var descriptionValue = document.getElementById('description').value;
+      let datepickerValue = document.getElementById('datepicker').value;
+      datepickerValue = moment(datepickerValue).format('DD/MM/YYYY HH:mm');
+      let descriptionValue = document.getElementById('description').value;
 
-      // Hacer algo con los valores, por ejemplo, imprimirlos en la consola
+      // imprimirlos en la consola
       console.log("Fecha:", datepickerValue);
       console.log("Descripción:", descriptionValue);
+
+      // Crear un nuevo elemento de lista
+      let listItem = document.createElement('li');
+      listItem.className = 'list-group-item d-flex';
+
+      // Crear un elemento de checkbox
+      let checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+
+      // Crear un elemento de párrafo para el texto de la lista
+      let listItemText = document.createElement('p');
+      listItemText.className = 'task-text';
+      listItemText.innerText = datepickerValue + ': ' + descriptionValue;
+
+      // Agregar el checkbox y el elemento de texto al elemento de lista
+      listItem.appendChild(checkbox);
+      listItem.appendChild(listItemText);
+     
+
+      // Agregar el nuevo elemento de lista a la lista existente
+      taskList.appendChild(listItem);
+
 
       // Limpiar los campos de entrada
       document.getElementById('datepicker').value = '';
       document.getElementById('description').value = '';
+
+//ver el cambio en el checkbox para completar una tarea
+
+      checkbox.addEventListener('change', function() {
+        if (this.checked) {
+          listItemText.classList.add('completed');
+  
+          // Obtener la fecha y hora actual
+          var completedDatetime = new Date();
+          var formattedCompletedDatetime = moment(completedDatetime).format('DD/MM/YYYY HH:mm');
+  
+          // Crear un elemento de párrafo para mostrar la fecha y hora de finalización
+          var completedText = document.createElement('p');
+          completedText.className = 'task-completed';
+          completedText.innerText = 'Completado el: ' + formattedCompletedDatetime;
+  
+          // Agregar el elemento de fecha y hora de finalización debajo del elemento de texto de la tarea
+          listItem.appendChild(completedText);
+        } else {
+          listItemText.classList.remove('completed');
+  
+          // Remover el elemento de fecha y hora de finalización si el checkbox no está marcado
+          var completedText = listItem.querySelector('.task-completed');
+          if (completedText) {
+            listItem.removeChild(completedText);
+          }
+        }
+      });
     });
   });
