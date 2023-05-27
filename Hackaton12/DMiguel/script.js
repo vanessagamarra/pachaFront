f53055a25bf71552ff4aade888ff40dfe2a61c87
pana-miguel <miguelangel.dominguez@unheval.pe>
2023-05-27 14:49:13 -0500

    se termino el reto 3, se crearon la funcion open y close que cambia si se ve o no se el modal

--- Hackaton12/DMiguel/script.js
+++ Hackaton12/DMiguel/script.js
@@ -7,8 +7,19 @@ function toggleDropdown() {
 
 // Reto 2
 function toggleCollapGroup(identificador = "") {
-    const identificar = "."+identificador;
+    const identificar = "." + identificador;
     var collapGroup = document.querySelector(identificar);
     collapGroup.classList.toggle('show');
 }
 
+// Reto 3
+
+function openModal() {
+    var modal = document.getElementById("exampleModal");
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    var modal = document.getElementById("exampleModal");
+    modal.style.display = "none";
+}
