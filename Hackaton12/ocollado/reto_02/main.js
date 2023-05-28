b3710927072914e82bafa9bd51a80884cb7b4566
Omar Augusto Collado Vargas <PEX0000433@prosegur.com>
2023-05-27 23:25:01 -0500

    add hackaton 12 - ocollado

---
+++ Hackaton12/ocollado/reto_02/main.js
@@ -0,0 +1,52 @@
+function Accordion(accordionElement) {
+    this.accordionElement = accordionElement;
+    this.accordionHeader = accordionElement.querySelector('.accordion-header');
+    this.accordionContent = accordionElement.querySelector('.accordion-content');
+
+    this.accordionHeader.addEventListener('click', this.toggle.bind(this));
+  }
+
+
+  Accordion.prototype.toggle = function() {
+
+    if (this.accordionContent.style.display === '') {
+      this.accordionContent.style.display = 'block';
+      return;
+  }
+
+    if (this.accordionContent.style.display === 'none') {
+      this.show();
+      this.closeOthers();
+    } else {
+      this.hide();
+    }
+  };
+
+
+  Accordion.prototype.show = function() {
+    this.accordionContent.style.display = 'block';
+  };
+
+
+  Accordion.prototype.hide = function() {
+    this.accordionContent.style.display = 'none';
+  };
+
+
+  Accordion.prototype.closeOthers = function() {
+    var allAccordions = document.querySelectorAll('.accordion');
+    allAccordions.forEach(function(accordion) {
+      if (accordion !== this.accordionElement) {
+        var accordionContent = accordion.querySelector('.accordion-content');
+        accordionContent.style.display = 'none';
+      }
+    }, this);
+  };
+
+
+  var accordionElements = document.querySelectorAll('.accordion');
+
+
+  accordionElements.forEach(function(accordionElement) {
+    new Accordion(accordionElement);
+  });
